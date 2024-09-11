ec61537acc75bef2da556567d8faae8372e58abc
KarolSamayoa <133280731+KarolSamayoa@users.noreply.github.com>
2024-09-11 16:38:54 -0400

    Add files via upload

---
+++ Chapter4_Loops_and_Files/ChapterExercises/WhileLoop.java
@@ -0,0 +1,18 @@
+/**
+ * This program demonstrate the while loop. 
+ */
+public class WhileLoop 
+{
+    public static void main (String[] args) 
+    {
+        int number = 1;
+        
+        while (number <= 5)
+        {
+            System.out.println("Hello");
+            number++;
+        }
+        
+        System.out.println("That's all!");
+    }
+}
