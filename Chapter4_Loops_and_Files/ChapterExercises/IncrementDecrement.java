3b41ceb5146213f5f260e04a1ef2254f48ef9fc7
KarolSamayoa <133280731+KarolSamayoa@users.noreply.github.com>
2024-09-11 15:31:12 -0400

    Add files via upload

---
+++ Chapter4_Loops_and_Files/ChapterExercises/IncrementDecrement.java
@@ -0,0 +1,27 @@
+/** 
+ * This program demonstrate the ++ and -- operators.
+ */
+package com.mycompany.loops_and_files; //DO NOT USE THIS LINE
+
+public class IncrementDecrement 
+{
+    public static void main(String[] args)
+    {
+        int number = 4;
+        
+        //Display the value in number
+        System.out.println("number is " + number);
+        System.out.println("I will increment number.");
+        
+        //Increment number
+        number++;
+        
+        System.out.println("Now, number is " + number);
+        System.out.println("I will decrement number.");
+        
+        //Decrement number
+        number--;
+        
+        System.out.println("Now, number is " + number);      
+     }
+}
