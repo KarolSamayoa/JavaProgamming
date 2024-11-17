db2cbd61bfcd73ca81669dfe08ef173f4925481e
KarolSamayoa <133280731+KarolSamayoa@users.noreply.github.com>
2024-11-17 18:10:44 -0500

    Add files via upload

---
+++ WrapperClass/SplitDemo1.java
@@ -0,0 +1,12 @@
+public class SplitDemo1 
+{
+    public static void main (String[] args) 
+    {
+        String str = "one two three four";
+        
+        String[] tokens = str.split(" ");
+        
+        for (String s : tokens)
+            System.out.println(s);
+    }
+}
